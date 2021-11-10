7192cd84ff1a09c224c524f83da36e57e37b6180
Rafael Galani <galani.rafael@gmail.com>
2021-11-10 14:38:53 -0300

    Cleanup

--- src/index.ts
+++ src/index.ts
@@ -10,28 +10,9 @@ const start = async (client = new Client(void 0, void 0, void 0)) => {
     console.log('[DEV]', color('Red Emperor', 'yellow'))
     console.log('[CLIENT] CLIENT Started!')
 
-    // await client.getPage().evaluate( ({
-    //     newReply, 
-    //     sendReplyWithMentions,
-    // }) => {
-    //     // eval(newReply)
-    //     // eval(sendReplyWithMentions)
-
-    //     // eval('window.WAPI.reply = newReply')
-    //     // eval('window.WAPI.sendReplyWithMentions = sendReplyWithMentions')
-    // }, {
-    //     newReply: newReply.toString() , 
-    //     sendReplyWithMentions: sendReplyWithMentions.toString() ,
-    // }); 
-    
-    // console.log('[CLIENT] INJECTED EXTERNAL LIB XDDDDDDDDDDDDDDDDDDDDDDDDDDD!')
-
-    // Message log for analytic
-    //client.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
-
     // Force it to keep the current session
     client.onStateChanged((state) => {
-        //console.log('[Client State]', state)
+        //console.log('[Client State]', state)``
         if (state === 'CONFLICT' || state === 'DISCONNECTED') client.forceRefocus()
     })
 
@@ -48,50 +29,15 @@ const start = async (client = new Client(void 0, void 0, void 0)) => {
         client.getGroupMembersId(id)
             .then((ids) => {
                 console.log('[CLIENT]', color(`Invited to Group. [ ${name} => ${ids.length}]`, 'yellow'))
-                // conditions if the group members are less than 10 then the bot will leave the group
-/*                 if (ids.length <= 10) {
-                    client.sendText(id, 'Sorry, the minimum group member is 10 user to use this bot. Bye~').then(() => client.leaveGroup(id))
-                } else {
-                    client.sendText(id, `Hello group members *${name}*, thank you for inviting this bot, to see the bot menu send *#menu*`)
-                } */
             }))
-
-    // listen paricipant event on group (wellcome message)
-    client.onGlobalParticipantsChanged(async (event) => {
-        // const host = await client.getHostNumber() + '@c.us'
-        // if (event.action === 'add' && event.who !== host) client.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
-    })
-
-    client.onIncomingCall((callData) => {
-        // client.contactBlock(callData.peerJid)
-    })
 }
 
 (async () => {
-    // const browser = await puppeteer.launch({
-    //     headless: false,
-    //     executablePath: require('chrome-launcher').Launcher.getInstallations()[0]
-    // });
-
-    // let injector = setInterval(async _ => {
-    //     let pages = await browser.pages();
-    //     let whatsAppPage = pages[0];
-
-    //     if (whatsAppPage.url().includes('whatsapp')){
-    //         whatsAppPage.evaluate( _ => {
-    //             window.Object.freeze = o => o;
-    //         })
-    //         clearInterval(injector);
-    //     }
-
-    // }, 50);
-
     const options = {
         sessionId: 'botzim',
         headless: false,
         qrTimeout: 0,
         authTimeout: 0,
-        // browserWSEndpoint: browser.wsEndpoint(),
         devtools: false,
         useChrome: true, 
         killProcessOnBrowserClose: true,
@@ -99,6 +45,6 @@ const start = async (client = new Client(void 0, void 0, void 0)) => {
     }
     
     create(options)
-        .then((client) => start(client))
+        .then((client) => start(client)  )
         .catch((err) => console.error(err))
 })()
