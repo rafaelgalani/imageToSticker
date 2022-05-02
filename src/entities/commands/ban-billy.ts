21914b74d1798715241b94caf08504241daf285a
Rafael Galani <galani.rafael@gmail.com>
2022-05-02 16:44:11 -0300

    Reorganize command imports and add back ban billy command

--- src/entities/commands/ban-billy.ts
+++ src/entities/commands/ban-billy.ts
@@ -1,40 +1,40 @@
-// import { GroupOnlyRule, NArgumentsRule } from "src/entities/rules";
-// import { ArgsOperator } from "src/entities/rules/group/n-arguments";
-// import { ZapCommand } from "./command";
-// import { randomInt } from "src/utils"
-// import { parse } from "dotenv";
+import { GroupOnlyRule, NArgumentsRule } from "src/entities/rules";
+import { ArgsOperator } from "src/entities/rules/group/n-arguments";
+import { ZapCommand } from "./command";
+import { loadJSON, randomInt, saveJSON } from "src/utils"
 
-// export class BanBillyCommand extends ZapCommand {
+export class BanBillyCommand extends ZapCommand {
     
-//     protected getPatterns(){
-//         return ['xd'];
-//     }
+    public getPatterns(){
+        return ['xd'];
+    }
 
-//     protected getRules(){
-//         return [ 
-//             new GroupOnlyRule(),
-//             new NArgumentsRule({ target: 1, operation: ArgsOperator.EQ }).override('Só um número por vez caralho!'), 
-//         ];
-//     }
+    protected getRules(){
+        return [ 
+            new GroupOnlyRule(),
+            new NArgumentsRule({ target: 1, operation: ArgsOperator.EQ }).override('Só um número por vez caralho!'), 
+        ];
+    }
 
-//     protected async runSpecificLogic() {
-//         if (!number) {
-//           number = randomInt(10000);
-//         }
-//         const { args, sender, client, groupId } = this.context;
-//         const nick = `@${sender.id.replace('@c.us', '')}`
-//         const guess = parseInt(args[0]);
-//         const billy = '5511958795261@c.us'
-//         if (guess > number) {
-//           return this.context.reply(`O ${nick} palpitou ${guess}: A resposta está ⬇`);
-//         }
-//         if (guess < number) {
-//           return this.context.reply(`O ${nick} palpitou ${guess}: A resposta está ⬆️`);
-//         }
-//         if (guess === number) {
-//           await this.context.reply(`O ${nick} ACERTOU! ADIVINHA SÓ? BILLY BANIDO IMEDIATAMENTE!`);
-//           await client.removeParticipant(groupId, billy);
-//           number = false;
-//         }
-//     }
-// }
+    protected async runSpecificLogic() {
+        const { args, sender, client, groupId } = this.context;
+        let number = loadJSON<number>( `bill-ban-${groupId}` );
+        if (! number ) {
+            number = randomInt( 10000 );
+            saveJSON( `bill-ban-${groupId}`, number );
+        }
+        const nick = `@${sender.id.replace('@c.us', '')}`
+        const guess = parseInt(args[0]);
+        const billy = '5511958795261@c.us'
+        if (guess > number) {
+          return this.context.reply(`O ${nick} palpitou ${guess}: A resposta está ⬇`);
+        }
+        if (guess < number) {
+          return this.context.reply(`O ${nick} palpitou ${guess}: A resposta está ⬆️`);
+        }
+        if (guess === number) {
+          await this.context.reply(`O ${nick} ACERTOU! ADIVINHA SÓ? BILLY BANIDO IMEDIATAMENTE!`);
+          await client.removeParticipant(groupId, billy);
+        }
+    }
+}
